35c85305d3dcfe76fc28341148641639aa69e6dc
vaninanton <vaninanton@users.noreply.github.com>
2023-06-04 21:06:09 +0000

    Fix styling

--- src/SpaceHealthcheckServiceProvider.php
+++ src/SpaceHealthcheckServiceProvider.php
@@ -28,7 +28,7 @@ class SpaceHealthcheckServiceProvider extends PackageServiceProvider
                     ->endWith(function (InstallCommand $command) {
                         $command->call(SpaceHealthcheckCommand::class);
                         $command->call('config:clear');
-                        $command->info('Open this link in browser: ' . route('space.check', ['secretKey' => config('space-healthcheck.secretKey')]));
+                        $command->info('Open this link in browser: '.route('space.check', ['secretKey' => config('space-healthcheck.secretKey')]));
                     });
             });
     }
